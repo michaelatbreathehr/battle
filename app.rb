fb9b4b74d69192e32ac3b64feb9d6ea73a3b11ec
Michael Redgrave <michael@breathehr.com>
2019-08-09 15:46:54 +0100

    add game class, remove attack from player and move it to game

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require_relative './lib/player'
+require_relative './lib/game'
+
 class Battle < Sinatra::Base
   enable :sessions
 
@@ -26,7 +28,7 @@ class Battle < Sinatra::Base
   get '/attack' do
     @player_1 = $player_1
     @player_2 = $player_2
-    @player_1.attack(@player_2)
+    Game.new.attack(@player_2)
     erb :attack
   end
 
--- lib/player.rb
+++ lib/player.rb
@@ -7,10 +7,10 @@ class Player
 		@hit_points = hit_points
 	end
 
-	def attack(player)
-		player.receive_damage
-	end
-  
+	# def attack(player)
+	# 	player.receive_damage
+	# end
+
 	def receive_damage
 		@hit_points -= 10
 	end
