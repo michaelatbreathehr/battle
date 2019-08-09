b44ef72c5d357cfcb7e360e45395a462cb47afc3
Michael Redgrave <michael@breathehr.com>
2019-08-09 14:14:15 +0100

    implementing hit points task - passing tests

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra/base'
-
+require_relative './lib/player'
 class Battle < Sinatra::Base
   enable :sessions
 
@@ -8,22 +8,26 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1_name] = params[:player_1_name]
-    session[:player_2_name] = params[:player_2_name]
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
+    # session[:player_1_name] = params[:player_1_name]
+    # session[:player_2_name] = params[:player_2_name]
     redirect '/play'
   end
 
   get '/play' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1 = $player_1.name
+    @player_2 = $player_2.name
+    # @player_1_name = session[:player_1_name]
+    # @player_2_name = session[:player_2_name]
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     erb :attack
   end
-
 
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,6 +1,8 @@
 feature 'View hit points' do
     scenario 'see Player 2 hit points' do
       sign_in_and_play
-      expect(page).to have_content 'Noah: 10HP'
+      click_button 'Attack'
+      click_button 'OK'
+      expect(page).to have_content "Mittens:50HP"
     end
   end
